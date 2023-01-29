1dbbbc7a020ec8c48b832e132dc9ef3da111bc42
TaewonBae <43931412+TaewonBae@users.noreply.github.com>
2023-01-29 23:16:48 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,4 +1,4 @@
-chess = [1,1,2,2,2,8]
-chess2 = list(map(int, input().split()))
+chess = [1,1,2,2,2,8] # 정해진 말의 수
+chess2 = list(map(int, input().split())) # 입력 받은 말의 수
 for i in range(6):
     print(chess[i]-chess2[i], end=' ')
